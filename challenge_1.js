f1c984d75de65c629f71f23fb12d15584b74c544
utsavgohel-simform <utsav@simformsolutions.com>
2023-01-19 17:57:54 +0530

    completed coding challenge 3

--- challenge_1.js
+++ challenge_1.js
@@ -48,3 +48,67 @@ const markHigherBMI2 = Mark2Mass>John2Mass
 
 console.log(markHigherBMI);
 
+/*Coding Challenge #3
+/*There are two gymnastics teams, Dolphins and Koalas. They compete against each
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks �
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+*/
+
+//Data 1
+const DolphinsAvgScore = (96+108+89)/3
+console.log(DolphinsAvgScore);
+
+const KoalasAvgScore = (88+91+110)/3
+console.log(KoalasAvgScore);
+
+if(DolphinsAvgScore>KoalasAvgScore){
+    console.log('Dolphins Wins');
+}else if(DolphinsAvgScore==KoalasAvgScore){
+    console.log('Match draw');
+}else{
+    console.log('Koalas Wins');
+}
+
+//Data Bonus 1:
+const Dolphins2AvgScore = (97+112+101)/3
+console.log(DolphinsAvgScore);
+
+const Koalas2AvgScore = (109+95+123)/3
+console.log(KoalasAvgScore);
+
+if(Dolphins2AvgScore>Koalas2AvgScore){
+    console.log('Dolphins Wins');
+}else if(Dolphins2AvgScore==Koalas2AvgScore){
+    console.log('Match draw');
+}else{
+    console.log('Koalas Wins');
+}
+//Data Bonus 2:
+const Dolphins3AvgScore = (97+112+101)/3
+console.log(DolphinsAvgScore);
+
+const Koalas3AvgScore = (109+95+106)/3
+console.log(KoalasAvgScore);
+
+if(Dolphins3AvgScore>Koalas3AvgScore){
+    console.log('Dolphins Wins');
+}else if(Dolphins3AvgScore==Koalas3AvgScore){
+    console.log('Match draw');
+}else{
+    console.log('Koalas Wins');
+}
