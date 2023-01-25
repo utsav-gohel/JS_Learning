443746db1b2fd6e2b32571bdb1260606c98fcac7
utsavgohel-simform <utsav@simformsolutions.com>
2023-01-25 13:08:57 +0530

    completed game logic

--- script.js
+++ script.js
@@ -20,11 +20,13 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector(".guess").value = " ";
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".highscore").textContent = "0";
 });
 
 const SecretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
+let highscore = 0;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
@@ -39,6 +41,10 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "Correct Number";
     document.querySelector("body").style.backgroundColor = "#60b347"; //here we are not using (. dot) as body is not class.
     document.querySelector(".number").style.width = "30rem";
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
   }
   //when guess is too high
   else if (guess > SecretNumber) {
