47467aaa2198c23a3c9912b1555e901a3340d824
utsavgohel-simform <utsav@simformsolutions.com>
2023-01-31 12:02:19 +0530

    upto promises methods

--- async.js
+++ async.js
@@ -68,20 +68,20 @@
 // };
 // greet("utsav", enrollno(123456));
 
-let promise = new Promise((resolve, reject) => {
-  console.log("inside promise");
-  setTimeout(() => {
-    // console.log("resolved promise in 2 sec");
-    resolve("resolved promise in 2 sec");
-    // reject("reject promise in 2 sec");
-  }, 2000);
-})
-  .then((val) => {
-    console.log(val);
-  })
-  .then((val) => {
-    console.log(val);
-  });
+// let promise = new Promise((resolve, reject) => {
+//   console.log("inside promise");
+//   setTimeout(() => {
+//     // console.log("resolved promise in 2 sec");
+//     resolve("resolved promise in 2 sec");
+//     // reject("reject promise in 2 sec");
+//   }, 2000);
+// })
+//   .then((val) => {
+//     console.log(val);
+//   })
+//   .then((val) => {
+//     console.log(val);
+//   });
 // .catch((err) => {
 //   console.log(err);
 // });
@@ -102,4 +102,76 @@ let promise = new Promise((resolve, reject) => {
 //   });
 // promise2.catch((err) => {
 //   console.log(err);
+// });
+
+//If we want to get value after every promise resolve then we use promise.all function And this will execute within maximum ms like
+//  if one promise have maximum 3000 ms then all promise resolve within 3000 ms
+// let prom1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("promise 1 after 1 sec"); // if we comment this promise then promise.all not going to return any value
+//     // reject("promise 1 after 1 sec"); // if we comment this promise then promise.all not going to return any value
+//   }, 1000);
+// });
+// let prom2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("promise 2 after 2 sec");
+//     // reject("promise 2 after 2 sec");
+//   }, 2000);
+// });
+// let prom3 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("promise 3 after 3 sec");
+//   }, 3000);
+// });
+
+//If any of promise will reject then promise.all not going to work
+// let all_promise = Promise.all([prom1, prom2, prom3]).then((val) => {
+//   console.log(val);
+// });
+
+//this will return 2 states if promise rejected then status will be rejected and return it's value..
+// let all_promise = Promise.allSettled([prom1, prom2, prom3]).then((val) => {
+//   console.log(val);
+// });
+
+//this will return first promise whichever execute first whether it's error/value.
+// let all_promise = Promise.race([prom1, prom2, prom3]).then((val) => {
+//   console.log(val);
+// });
+
+//It will execute only first resolved promise, it not return any reject event.
+// let all_promise = Promise.any([prom1, prom2, prom3]).then((val) => {
+//   console.log(val);
+// });
+
+// let all_promise = Promise.resolve(1).then((val) => {
+//   console.log(val);
+// });
+
+// let all_promise = Promise.reject(new Error("error in promises")).then((val) => {
+//   console.log(val);
+// });
+
+// let a = document.getElementById("btn");
+// a.addEventListener("click", () => {
+//   setTimeout(() => {
+//     // document.body.style.backgroundColor = "red";
+//   }, 1000);
+//   // document.body.style.backgroundColor = "red"
+// });
+
+let button = document.getElementById("btn");
+let id1 = document.getElementById("id1");
+const blue = () => {
+  document.getElementById("btn").style.backgroundColor = "blue";
+};
+const red = () => {
+  // document.getElementById("btn").style.backgroundColor = "blue";
+  document.getElementById("id1").style.backgroundColor = "red";
+  // document.body.style.backgroundColor = "red";
+};
+
+// button.addEventListener("click", blue);
+// id1.addEventListener("click", () => {
+//   document.getElementById("id1").classList.toggle("visible");
 // });
